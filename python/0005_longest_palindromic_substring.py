d4d153899229a5b8d7553122ddde33c62916e53e
linmufeng <mufeng.lin@transwarp.io>
2020-08-11 20:21:43 +0800

    python 0130

--- python/0005_longest_palindromic_substring.py
+++ python/0005_longest_palindromic_substring.py
@@ -1,32 +1,22 @@
+from collections import defaultdict
+
+
 # 660ms
 def longestPalindrome(s: str):
+    if not s:
+        return ''
+    max_pal = s[0]
+    max_l = 1
+    c_dict = defaultdict(list)
     i = 0
-    c_dict = {}
-    # pal_list = []
-    max_pal = ""
-    max_l = 0
     while i < len(s):
-        if c_dict.get(s[i]):
-            for c_ind in c_dict.get(s[i]):
-                if s[c_ind: i + 1][::-1] == s[c_ind: i + 1]:
-                # if (c_ind == i - 1 or
-                #         c_ind == i - 2 or
-                #         (c_ind == i - 3 and
-                #          s[c_ind + 1] == s[i - 1]) or
-                #         (c_ind + 1, i - 1) in pal_list or
-                #         s[c_ind: i + 1][::-1] == s[c_ind: i + 1]):
-                #     pal_list.append((c_ind, i))
-                    if i - c_ind + 1 > max_l:
-                        max_l = i - c_ind + 1
-                        max_pal = s[c_ind: i + 1]
-                    break
-            c_dict.get(s[i]).append(i)
-        else:
-            c_dict[s[i]] = [i]
-            # pal_list.append((i, i))
-            if max_l == 0:
-                max_l = 1
-                max_pal = s[i]
+        for c_ind in c_dict[s[i]]:
+            if s[c_ind: i + 1][::-1] == s[c_ind: i + 1]:
+                if i - c_ind + 1 > max_l:
+                    max_l = i - c_ind + 1
+                    max_pal = s[c_ind: i + 1]
+                break
+        c_dict[s[i]].append(i)
         i += 1
     return max_pal
 
@@ -50,7 +40,7 @@ def longestPalindrome1(s: str):
     return s[start: end + 1]
 
 
-print(longestPalindrome1("adda"))
+print(longestPalindrome("adda"))
 print(longestPalindrome("avaddsddaa"))
 print(longestPalindrome("as"))
 print(longestPalindrome("aaaaasaaaa"))
